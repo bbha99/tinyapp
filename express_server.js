a993d1de2ac942a954765b84703da1a5e9ddb56e
bbha99 <branha99@gmail.com>
2023-01-25 01:50:52 -0800

    utilized the cookie parser to identify user

--- express_server.js
+++ express_server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+var cookieParser = require('cookie-parser');
+
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -17,6 +19,7 @@ app.set("view engine", "ejs");
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // Database
 const urlDatabase = {
@@ -31,17 +34,22 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, 
+    username: req.cookies["username"]
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { urls: urlDatabase, 
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
   const shortURL = req.params.id;
-  const templateVars = { id: shortURL, longURL: urlDatabase[shortURL] };
+  const templateVars = { id: shortURL, longURL: urlDatabase[shortURL], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
@@ -77,6 +85,11 @@ app.post('/urls/:id', (req, res) => {
 app.post("/urls/:id/delete", (req, res) => {
 
   delete urlDatabase[req.params.id];
+  res.redirect('/urls');
+});
+
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
   res.redirect('/urls');
 });
 
